31395610bc105148f338213616aa1f408e5b4f30
Shwet <shwetpatel2712@gmail.com>
2024-05-10 16:27:32 +0530

    Set-up basic structure

---
+++ cruncher/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <div className=" bg-green-100 min-h-screen py-16">
+      <div className=" bg-neutral-50  grid grid-cols-2  min-h-[75vh]  mx-auto max-w-6xl  shadow-xl rounded-lg">
+        <div className=" bg-slate-100  max-w-[25%]">
+          col-1
+        </div>
+        <div className=''>
+          col-2
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
